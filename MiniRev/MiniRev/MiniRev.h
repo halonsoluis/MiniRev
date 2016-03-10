15c0afca11c1245206b81c55b9da6dcfde1a6f7d
Hugo Alonso <hugoalonsoluis@gmail.com>
2016-03-10 16:56:30 -0500

    Create initial empty bundle

---
+++ MiniRev/MiniRev/MiniRev.h
@@ -0,0 +1,19 @@
+//
+//  MiniRev.h
+//  MiniRev
+//
+//  Created by Hugo on 3/10/16.
+//  Copyright © 2016 halonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MiniRev.
+FOUNDATION_EXPORT double MiniRevVersionNumber;
+
+//! Project version string for MiniRev.
+FOUNDATION_EXPORT const unsigned char MiniRevVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniRev/PublicHeader.h>
+
+
